1b721cc16d623939b282c13cfd3f04410bb182f4
henriquesbarros19 <henrique.barros@compasso.com.br>
2022-08-01 17:16:58 -0300

    feat: add fonts and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <h1>hello world</h1>
     </ThemeProvider>
   )
