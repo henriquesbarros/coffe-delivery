2fead33d8bdb7fef8309edd808bb75e83fb8984a
henriquesbarros <henriquesbarroshs@gmail.com>
2022-08-12 17:44:02 -0300

    remove github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: 'https://henriquesbarros.github.io/coffe-delivery/',
   plugins: [react()],
 })
